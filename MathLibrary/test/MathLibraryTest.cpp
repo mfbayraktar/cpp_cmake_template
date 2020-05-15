f71e8d2962cdf5954fc7ff6b4c74ba5b9456e96a
Muhammed Fatih Bayraktar <mfbayraktar@live.com>
2020-05-16 02:54:03 +0300

    Simple Test Added For Math Library

--- MathLibrary/test/MathLibraryTest.cpp
+++ MathLibrary/test/MathLibraryTest.cpp
@@ -1,5 +1,6 @@
- #include <gtest/gtest.h>
+#include <gtest/gtest.h>
+#include "arithmetic.hpp"
 
- TEST(MathLibraryTests, MacroTest) {
-     EXPECT_TRUE(true);
- }
+TEST(MathLibraryTests, MacroTest) {
+    EXPECT_EQ(multiply(10, 20), 200);
+}
